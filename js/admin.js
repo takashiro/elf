5d992f5a99af98585dacaf3e2dfeaf3491c20905
Takashiro <takashiro@qq.com>
2013-12-17 23:49:03 +0800

    Add a new option to refresh cached css & js
    Signed-off-by: Takashiro <takashiro@qq.com>

---
+++ js/admin.js
@@ -0,0 +1,18 @@
+function randomstr(length){
+	var str = '';
+	for(var i = 0; i < length; i++){
+		var rand = Math.floor(Math.random() * 62);
+		if(rand >= 0 && rand <= 25){
+			rand += 0x41;
+		}else if(rand <= 51){
+			rand -= 26;
+			rand += 0x61;
+		}else{
+			rand -= 52;
+			rand += 0x30;
+		}
+		str += String.fromCharCode(rand);
+	}
+
+	return str;
+}
