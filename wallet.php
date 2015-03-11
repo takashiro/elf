d90c241c55de877dcc2a576273d1f9ab23255331
Takashiro <takashiro@qq.com>
2015-03-11 16:31:36 +0800

    Fix wallet recharge log

--- wallet.php
+++ wallet.php
@@ -5,8 +5,8 @@ require_once './core/init.inc.php';
 $limit = 10;
 $offset = ($page - 1) * $limit;
 $db->select_table('userwalletlog');
-$pagenum = $db->RESULTF('COUNT(*)');
-$walletlog = $db->MFETCH('*', "1 ORDER BY dateline LIMIT $offset,$limit");
+$pagenum = $db->RESULTF('COUNT(*)', "uid=$_USER[id]");
+$walletlog = $db->MFETCH('*', "uid=$_USER[id] ORDER BY dateline DESC LIMIT $offset,$limit");
 
 $prepaidreward = $db->fetch_all("SELECT * FROM {$tpre}prepaidreward WHERE etime_start<=$timestamp AND etime_end>=$timestamp");
 foreach($prepaidreward as &$r){
