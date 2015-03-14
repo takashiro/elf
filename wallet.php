ef1b661c84aa67f687f2c1c164545413b143e370
Takashiro <takashiro@qq.com>
2015-03-14 23:42:52 +0800

    Enable users to pay for orders with orchardhut wallet

--- wallet.php
+++ wallet.php
@@ -2,6 +2,31 @@
 
 require_once './core/init.inc.php';
 
+if(isset($_GET['orderid'])){
+	$order = new Order($_GET['orderid']);
+	if(!$order->exists()){
+		showmsg('order_not_exist', 'back');
+	}
+
+	if(!empty($order->alipaystate)){
+		showmsg('your_alipay_wallet_is_processing_the_order', 'back');
+	}
+
+	if($_G['user']->wallet < $order->totalprice){
+		showmsg('wallet_is_insufficient', 'back');
+	}
+
+	$db->query("UPDATE {$tpre}user SET wallet=wallet-{$order->totalprice} WHERE id={$_G['user']->id} AND wallet>={$order->totalprice}");
+	if($db->affected_rows() <= 0){
+		showmsg('wallet_is_insufficient', 'back');
+	}
+
+	$order->paymentmethod = Order::PaidWithWallet;
+	$order->alipaystate = AlipayNotify::TradeSuccess;
+
+	showmsg('order_is_successfully_paid', 'back');
+}
+
 $limit = 10;
 $offset = ($page - 1) * $limit;
 $db->select_table('userwalletlog');
