1957ad5cf17376dffaab76197cc36fa96718c553
Takashiro <takashiro@qq.com>
2015-03-16 20:55:00 +0800

    Record stack trace when errors occur

--- controller/core_handleerror.inc.php
+++ controller/core_handleerror.inc.php
@@ -1,14 +1,22 @@
 <?php
 
+if(!defined('S_ROOT')) exit('access denied');
+
 $errorID = md5($errorLevel.$errorMessage.$errorFile.$errorLine);
-$filePath = S_ROOT.'data/log/'.$errorID.'_error.log.php';
+$filePath = S_ROOT.'data/error/'.$errorID.'.inc.php';
+
+$fileRootLength = strlen(S_ROOT) - 1;
+if(strncmp(S_ROOT, $errorFile, $fileRootLength) == 0){
+	$errorFile = substr($errorFile, $fileRootLength + 1);
+}
 
 if(!file_exists($filePath)){
 	global $PHP_SELF, $_G;
 
 	@$fp = fopen($filePath, 'w');
 	if($fp){
-		fwrite($fp, "Error Level: $errorLevel\r\n");
+		fwrite($fp, '<?php exit;?>');
+		fwrite($fp, "\r\nError Level: $errorLevel\r\n");
 		fwrite($fp, "Error Message: $errorMessage\r\n");
 		fwrite($fp, "Error File: $errorFile\r\n");
 		fwrite($fp, "Error Line: $errorLine\r\n");
@@ -20,15 +28,34 @@ if(!file_exists($filePath)){
 		}
 		fwrite($fp, "\r\n");
 
-		if($_GET){
-			fwrite($fp, "GET: \r\n");
-			fwrite($fp, var_export($_GET, true));
-			fwrite($fp, "\r\n");
-		}
+		fwrite($fp, "\r\nPOST: \r\n");
+		fwrite($fp, file_get_contents('php://input'));
+		fwrite($fp, "\r\n");
 
-		if($_POST){
-			fwrite($fp, "POST: \r\n");
-			fwrite($fp, var_export($_POST, true));
+		fwrite($fp, "\r\nStack Trace:\r\n");
+
+		$traces = debug_backtrace();
+		$tracenum = count($traces);
+		for($i = 2; $i < $tracenum; $i++){
+			$trace = &$traces[$i];
+			if(strncmp(S_ROOT, $trace['file'], $fileRootLength) == 0){
+				$trace['file'] = substr($trace['file'], $fileRootLength + 1);
+			}
+
+			fwrite($fp, "{$trace['file']}({$trace['line']}): ");
+			if(isset($trace['class'])){
+				fwrite($fp, $trace['class']);
+				if(isset($trace['type'])){
+					fwrite($fp, $trace['type']);
+				}else{
+					write($fp, '??');
+				}
+			}
+			if(isset($trace['function'])){
+				fwrite($fp, $trace['function']);
+				fwrite($fp, '()');
+				//@todo: record function arguments here
+			}
 			fwrite($fp, "\r\n");
 		}
 
