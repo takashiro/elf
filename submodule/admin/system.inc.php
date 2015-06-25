f1e69ab6c619a42cde096acfb847f5d85219e0d3
Takashiro <takashiro@qq.com>
2015-06-25 16:31:35 +0800

    系统设置模块更新

--- submodule/admin/system.inc.php
+++ submodule/admin/system.inc.php
@@ -23,58 +23,62 @@
 
 if(!defined('IN_ADMINCP')) exit('access denied');
 
-if($_G['admincp']['mode'] == 'permission'){
-	return array();
-}
+class SystemModule extends AdminControlPanelModule{
 
-@$config = !empty($_POST['system']) ? $_POST['system'] : null;
+	public function defaultAction(){
+		extract($GLOBALS, EXTR_SKIP | EXTR_REFS);
 
-@$config = array(
-	'sitename' => $config['sitename'],
-	'timezone' => intval($config['timezone']),
-	'timefix' => intval($config['timefix']),
-	'cookiepre' => $config['cookiepre'],
-	'refversion' => $config['refversion'],
-	'charset' => 'utf-8',
-	'style' => $config['style'],
-	'debugmode' => !empty($config['debugmode']),
-	'log_request' => !empty($config['log_request']),
-	'log_error' => !empty($config['log_error']),
-	'refresh_template' => !empty($config['refresh_template']),
-	'head_element' => htmlspecialchars_decode($config['head_element']),
-	'icp' => htmlspecialchars($config['icp']),
-	'jquery_cdn' => trim($config['jquery_cdn']),
-	'error_report_to' => trim($config['error_report_to']),
-);
+		@$config = !empty($_POST['system']) ? $_POST['system'] : null;
 
-if($_POST){
-	writedata('config', $config);
-	showmsg('successfully_updated_system_config', 'refresh');
-}
+		@$config = array(
+			'sitename' => $config['sitename'],
+			'timezone' => intval($config['timezone']),
+			'timefix' => intval($config['timefix']),
+			'cookiepre' => $config['cookiepre'],
+			'refversion' => $config['refversion'],
+			'charset' => 'utf-8',
+			'style' => $config['style'],
+			'debugmode' => !empty($config['debugmode']),
+			'log_request' => !empty($config['log_request']),
+			'log_error' => !empty($config['log_error']),
+			'refresh_template' => !empty($config['refresh_template']),
+			'head_element' => htmlspecialchars_decode($config['head_element']),
+			'icp' => htmlspecialchars($config['icp']),
+			'jquery_cdn' => trim($config['jquery_cdn']),
+			'error_report_to' => trim($config['error_report_to']),
+		);
 
-foreach($config as $var => $v){
-	isset($_CONFIG[$var]) || $_CONFIG[$var] = $v;
-}
-
-$_G['stylelist'] = array(
-	'admin' => array(),
-	'user' => array(),
-);
-foreach($_G['stylelist'] as $template_type => &$stylelist){
-	$styledir = S_ROOT.'view/'.$template_type.'/';
-	$view = opendir($styledir);
-	while($style = readdir($view)){
-		if($style{0} == '.'){
-			continue;
+		if($_POST){
+			writedata('config', $config);
+			showmsg('successfully_updated_system_config', 'refresh');
 		}
 
-		if(is_dir($styledir.$style)){
-			$stylelist[$style] = $style;
+		foreach($config as $var => $v){
+			isset($_CONFIG[$var]) || $_CONFIG[$var] = $v;
 		}
+
+		$_G['stylelist'] = array(
+			'admin' => array(),
+			'user' => array(),
+		);
+		foreach($_G['stylelist'] as $template_type => &$stylelist){
+			$styledir = S_ROOT.'view/'.$template_type.'/';
+			$view = opendir($styledir);
+			while($style = readdir($view)){
+				if($style{0} == '.'){
+					continue;
+				}
+
+				if(is_dir($styledir.$style)){
+					$stylelist[$style] = $style;
+				}
+			}
+		}
+		unset($stylelist);
+
+		include view('system');
 	}
-}
-unset($stylelist);
 
-include view('system');
+}
 
 ?>
