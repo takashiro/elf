a5d4ce2628070370b403fc96f6a59243c16c8692
Takashiro <takashiro@qq.com>
2015-07-07 16:01:45 +0800

    将点击事件的id作为关键词使用

--- api/weixin_server.php
+++ api/weixin_server.php
@@ -34,18 +34,19 @@ if(!$request){
 	exit('access denied');
 }
 
+$targetKeyword = '';
 if($request['MsgType'] == 'event'){
 	if($request['Event'] == 'subscribe'){
 		$weixin->replyTextMessage($wx['subscribe_text']);
-	}
-	//click menu,  to-do "KEY"
-	if($request['Event'] == 'CLICK'){
-		$user = $request['FromUserName'];
-		$key = Authkey::Generate($user);
-		$weixin->replyTextMessage("<a href=\"{$_G['root_url']}weixinconnect.php?action=login&user=$user&key=$key\">".lang('message', 'click_and_enter_shop').'</a>');
+	}elseif($request['Event'] == 'CLICK'){
+		$targetKeyword = $request['EventKey'];
 	}
 }elseif($request['MsgType'] == 'text'){
-	if(isset($wx['bind_keyword']) && Autoreply::MatchKeywords($wx['bind_keyword'], $request['Content'])){
+	$targetKeyword = $request['Content'];
+}
+
+if(!empty($targetKeyword)){
+	if(isset($wx['bind_keyword']) && Autoreply::MatchKeywords($wx['bind_keyword'], $targetKeyword)){
 		$words = explode("\n", $wx['bind2_keyword']);
 		foreach($words as &$word){
 			$word = trim($word);
@@ -62,19 +63,19 @@ if($request['MsgType'] == 'event'){
 		$weixin->replyTextMessage(lang('message', 'if_not_logged_in_comma')."<a href=\"{$_G['root_url']}memcp.php?action=login\">".lang('message', 'click_here_and_login')."</a>，".lang('message', 'and_reply').$words);
 	}
 
-	if(isset($wx['bind2_keyword']) && Autoreply::MatchKeywords($wx['bind2_keyword'], $request['Content'])){
+	if(isset($wx['bind2_keyword']) && Autoreply::MatchKeywords($wx['bind2_keyword'], $targetKeyword)){
 		$user = $request['FromUserName'];
 		$key = Authkey::Generate($user);
 		$weixin->replyTextMessage("<a href=\"{$_G['root_url']}weixinconnect.php?action=bind&user=$user&key=$key\">".lang('message', 'click_and_enter_shop').'</a>');
 	}
 
-	if(isset($wx['entershop_keyword']) && Autoreply::MatchKeywords($wx['entershop_keyword'], $request['Content'])){
+	if(isset($wx['entershop_keyword']) && Autoreply::MatchKeywords($wx['entershop_keyword'], $targetKeyword)){
 		$user = $request['FromUserName'];
 		$key = Authkey::Generate($user);
 		$weixin->replyTextMessage("<a href=\"{$_G['root_url']}weixinconnect.php?action=login&user=$user&key=$key\">".lang('message', 'click_and_enter_shop').'</a>');
 	}
 
-	$reply = Autoreply::Find($request['Content']);
+	$reply = Autoreply::Find($targetKeyword);
 	if($reply != NULL){
 		$weixin->replyTextMessage($reply);
 	}
