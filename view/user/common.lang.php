1820d6b5c265a1f3b2d614ffc38bd49e2a8f9fce
Takashiro <takashiro@qq.com>
2015-10-22 19:19:12 +0800

    更新语言包

--- view/user/common.lang.php
+++ view/user/common.lang.php
@@ -4,48 +4,12 @@ return array(
 	'comma' => '，',
 	'period' => '。',
 
-	'order' => '订单',
-	'order_id' => '订单编号',
-	'order_detail' => '订单详情',
-	'order_unsorted' => '待出库',
-	'order_sorted' => '已打包',
-	'order_to_delivery_station' => '发车在途',
-	'order_in_delivery_station' => '到配送站',
-	'order_delivering' => '送货中',
-	'order_received' => '已签收',
-	'order_rejected' => '已拒收',
-	'order_canceled' => '已取消',
-
-	'order_paidwithcash' => '货到付现',
-	'order_paidwithalipay' => '支付宝',
-	'order_paidwithwallet' => '钱包余额',
-	'order_paidwithbestpay' => '翼支付',
-
-	'order_waitbuyerpay' => '待付款',
-	'order_tradeclosed' => '关闭',
-	'order_tradesuccess' => '成功',
-	'order_tradepending' => '待收款',
-	'order_tradefinished' => '结束',
-
-	'home_delivery' => '送货上门',
-	'station_delivery' => '自提点取货',
-
 	'mobile' => '手机',
 
 	'recharge' => '充值',
-	'order_refund' => '退款',
-	'order_payment' => '支付',
+	'refund' => '退款',
+	'payment' => '支付',
 	'transfer' => '转账',
-
-	'out_of_stock' => '缺货',
-
-	'returned_order_submitted' => '已提交',
-	'returned_order_handled' => '处理中',
-	'returned_order_confirmed' => '处理完毕',
-	'returned_order_unhandled_detail' => '未处理',
-	'returned_order_invalid_detail' => '条件不符',
-	'returned_order_fee_only' => '退款',
-	'returned_order_fee_and_item' => '退货退款',
 );
 
 ?>
