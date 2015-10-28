61ed521ce54810e16552dc38b1464874e5ad306a
Takashiro <takashiro@qq.com>
2015-10-28 10:19:54 +0800

    Update sha1.sh to exclude .gitignore and sha1.sh

--- sha1.sh
+++ sha1.sh
@@ -1,8 +1,8 @@
 echo '<?php exit;?>' > data/sha1.inc.php
-git ls-files -s | cut -c8-47,50- >> data/sha1.inc.php
+git ls-files -s | grep -v ".gitignore" | grep -v "sha1.sh" | cut -c8-47,50- >> data/sha1.inc.php
 
 if [ -d "extension" ]; then
 	cd extension
-	git ls-files -s | cut -c8-47,50- | sed 's/\t/\textension\//g' >> ../data/sha1.inc.php
+	git ls-files -s | grep -v ".gitignore" | cut -c8-47,50- | sed 's/\t/\textension\//g' >> ../data/sha1.inc.php
 	cd ../
 fi
