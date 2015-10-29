157b3958c7770052fb5935b3bfeec79abf4d3bec
Takashiro <takashiro@qq.com>
2015-10-29 19:55:48 +0800

    Remove unnecessary function wrapping

--- module/alipay/config.inc.php
+++ module/alipay/config.inc.php
@@ -34,10 +34,10 @@ $alipay_config['ali_public_key_path'] = S_ROOT.'data/alipay_public_key_'.$alipay
 //↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
 //签名方式 不需修改
-$alipay_config['sign_type'] = strtoupper('RSA');
+$alipay_config['sign_type'] = 'RSA';
 
 //字符编码格式 目前支持 gbk 或 utf-8
-$alipay_config['input_charset'] = strtolower('utf-8');
+$alipay_config['input_charset'] = 'utf-8';
 
 //ca证书路径地址，用于curl中ssl校验
 //请保证cacert.pem文件在当前文件夹目录中
