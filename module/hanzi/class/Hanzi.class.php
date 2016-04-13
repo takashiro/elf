fc781b2e6eafa0c0494ee477f087f8f6a371163c
Takashiro <takashiro@qq.com>
2016-04-13 21:08:42 +0800

    Add Hanzi module

---
+++ module/hanzi/class/Hanzi.class.php
@@ -0,0 +1,57 @@
+<?php
+
+/***********************************************************************
+Elf Web App
+Copyright (C) 2013-2015  Kazuichi Takashiro
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+takashiro@qq.com
+************************************************************************/
+
+class Hanzi{
+	static public function ToCapital($str){
+		$chars = self::utf8_str_split($str);
+
+		$capitals = array();
+		global $db;
+		$table = $db->select_table('pinyin');
+		$rows = $table->fetch_all('hanzi,capital', 'hanzi IN (\''.implode('\',\'', $chars).'\')');
+		foreach($rows as $r){
+			$capitals[$r['hanzi']] = $r['capital'];
+		}
+
+		$result = '';
+		foreach($chars as $char){
+			if(isset($capitals[$char])){
+				$result.= $capitals[$char];
+			}elseif(preg_match('/\w/', $char)){
+				$result.= $char;
+			}
+		}
+		return $result;
+	}
+
+	private static function utf8_str_split($str,$split_len = 1){
+		if(!preg_match('/^[0-9]+$/', $split_len) || $split_len < 1){
+			return FALSE;
+		}
+		$len = mb_strlen($str, 'UTF-8');
+		if ($len <= $split_len){
+			return array($str);
+		}
+		preg_match_all('/.{'.$split_len.'}|[^\x00]{1,'.$split_len.'}$/us', $str, $ar);
+		return $ar[0];
+	}
+}
---
+++ module/hanzi/generate.php
@@ -0,0 +1,50 @@
+<?php
+
+/***********************************************************************
+Elf Web App
+Copyright (C) 2013-2015  Kazuichi Takashiro
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+takashiro@qq.com
+************************************************************************/
+
+
+require_once '../../core/init.inc.php';
+
+
+//Administrator automatically logs in via cookie records
+$_G['admin'] = new Administrator;
+$_G['admin']->login();
+
+//Handle user login or logout requests
+if(!$_G['admin']->isLoggedIn() || !$_G['admin']->isSuperAdmin()){
+	exit('access denied');
+}
+
+if(isset($_GET['table']) && isset($_GET['from']) && isset($_GET['to'])){
+	$table = $tpre.trim($_GET['table']);
+	$from = trim($_GET['from']);
+	$to = trim($_GET['to']);
+}else{
+	exit('invalid input');
+}
+
+$query = $db->query("SELECT `$from` FROM `$table`");
+while($row = $query->fetch_array()){
+	$capital = Hanzi::ToCapital($row[0]);
+	$db->query("UPDATE $table SET `$to`='$capital' WHERE `$from`='{$row[0]}'");
+}
+
+exit('ok');
