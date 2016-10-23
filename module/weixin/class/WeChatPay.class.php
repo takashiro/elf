f75630d59abdea67bc4dcc544c80a57819ae56d1
Takashiro <takashiro@qq.com>
2016-10-24 01:30:47 +0800

    Fix app pay notify_url

--- module/weixin/class/WeChatPay.class.php
+++ module/weixin/class/WeChatPay.class.php
@@ -72,6 +72,9 @@ class WeChatPay extends CUrl{
 			'total_fee' => round($total_fee * 100),
 			'trade_type' => $this->tradeType,
 		);
+		if($this->tradeType == 'APP'){
+			$data['notify_url'].= '?is_client=1';
+		}
 
 		$this->signData($data);
 
